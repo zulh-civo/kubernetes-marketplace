3644c6f1fad3d4852d2b1f763709aeee82b5b727
Zulhilmi Zainudin <zulh@civo.com>
2021-02-16 12:27:49 +0800

    Update cert-manager application

--- cert-manager/install.sh
+++ cert-manager/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# https://cert-manager.io/docs/installation/kubernetes/#installing-with-helm
 
 kubectl create namespace cert-manager
 
@@ -6,10 +7,9 @@ helm repo add jetstack https://charts.jetstack.io
 
 helm repo update
 
-kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.0.2/cert-manager.crds.yaml
+kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.1.0/cert-manager.crds.yaml
 
 helm install \
-  --name cert-manager \
+  cert-manager jetstack/cert-manager \
   --namespace cert-manager \
-  --version v1.0.2 \
-  jetstack/cert-manager
+  --version v1.1.0
--- cert-manager/uninstall.sh
+++ cert-manager/uninstall.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+# https://cert-manager.io/docs/installation/uninstall/kubernetes/#uninstalling-with-helm
 
 # Delete the app
-helm delete cert-manager -n=cert-manager
+helm --namespace cert-manager delete cert-manager
 
 # Delete the custom CRDs
-kubectl delete -f https://github.com/jetstack/cert-manager/releases/download/v1.0.2/cert-manager.crds.yaml
+kubectl delete -f https://github.com/jetstack/cert-manager/releases/download/v1.1.0/cert-manager.crds.yaml
 
 # Delete the namespace
 kubectl delete namespace cert-manager
