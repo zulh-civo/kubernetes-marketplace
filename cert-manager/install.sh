25e2791cc79fa66d715bfc8b4a9b54e3506a0cb4
Zulhilmi Zainudin <zulh@civo.com>
2021-04-05 15:55:47 +0800

    Update cert-manager app

--- cert-manager/install.sh
+++ cert-manager/install.sh
@@ -9,7 +9,7 @@ helm repo update
 
 kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v1.1.0/cert-manager.crds.yaml
 
-helm install \
+helm upgrade --install \
   cert-manager jetstack/cert-manager \
   --namespace cert-manager \
   --version v1.1.0
